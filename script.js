0709e9046721f714b982c53bcf53a67cc933c225
heather94 <98779837+heather94@users.noreply.github.com>
2022-10-16 20:35:15 +0100

    Code created by myself for codecademy challenge

---
+++ script.js
@@ -0,0 +1,29 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget= ()=> {
+  return secretTarget=Math.floor(Math.random()*10);
+}
+const compareGuesses = (humanGuess, computerGuess, secretTarget)=> {
+  const humanDifference = Math.abs(secretTarget-humanGuess);
+  const computerDifference = Math.abs(secretTarget-computerGuess);
+ return humanDifference <= computerDifference
+
+}
+const updateScore = winner => {
+if(winner==="human"){
+  humanScore++;
+}else if(winner==="computer"){
+  computerScore++;
+}
+}
+const advanceRound = () => {
+currentRoundNumber++;
+}
+const outOfRange = (humanGuess) => {
+  if (humanGuess > 9 ){
+    console.log(alert("You are out of range"));
+  }
+}
